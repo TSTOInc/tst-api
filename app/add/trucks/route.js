5293a79c0f22af278cbe2a1b3bd609dc5e7a94f3
Angel Dev <75772159+Angel-999@users.noreply.github.com>
2025-08-15 22:20:36 -0700

    fixed

--- app/add/trucks/route.js
+++ app/add/trucks/route.js
@@ -22,6 +22,13 @@ export async function POST(request) {
     const data = await request.json();
     const { image_url, truck_number, truck_alias, vin, make, model, year, transponder_id,  driver_id, status, color } = data;
 
+    if (!truck_number || !status) {
+        const missingFields = [];
+        if (!truck_number) missingFields.push('truck_number');
+        if (!status) missingFields.push('status');
+      return createCorsResponse({ error: 'Missing required fields: ' + missingFields.join(', ') + '.' }, 400);
+    }
+
     const insertText = `
       INSERT INTO drivers (image_url, truck_number, truck_alias, vin, make, model, year, transponder_id, driver_id, status, color)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
@@ -36,7 +43,7 @@ export async function POST(request) {
       make || null,
       model || null,
       year || null,
-      transponder_id || null,
+      trasponder_id || null,
       driver_id || null,
       status,
       color || null,
