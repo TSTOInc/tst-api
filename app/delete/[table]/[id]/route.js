f411cc81b6ed0a08fab6bad60b79ff3dbf328c8e
Angel Dev <75772159+Angel-999@users.noreply.github.com>
2025-08-16 00:27:57 -0700

    fixed

--- app/delete/[table]/[id]/route.js
+++ app/delete/[table]/[id]/route.js
@@ -9,7 +9,7 @@ const pool = new Pool({
 
 function createCorsResponse(data, status = 200) {
   const res = NextResponse.json(data, { status });
-  res.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.headers.set('Access-Control-Allow-Origin', '*');
   res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.headers.set('Access-Control-Max-Age', '86400');
@@ -39,7 +39,7 @@ export async function DELETE(req, { params }) {
 // OPTIONS handler for preflight
 export async function OPTIONS() {
     const res = new NextResponse(null, { status: 204 });
-    res.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.headers.set('Access-Control-Allow-Origin', '*');
     res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.headers.set('Access-Control-Max-Age', '86400'); // 1 day
