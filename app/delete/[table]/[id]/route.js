1cf6381d6b356d3e4c093ff6fad0cec523729a4a
Angel Dev <75772159+Angel-999@users.noreply.github.com>
2025-08-17 12:09:47 -0700

    delete image also

--- app/delete/[table]/[id]/route.js
+++ app/delete/[table]/[id]/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import pkg from 'pg';
 const { Pool } = pkg;
+import { del } from '@vercel/blob';
+
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -8,21 +10,41 @@ const pool = new Pool({
 });
 
 function createCorsResponse(data, status = 200) {
-  const res = NextResponse.json(data, { status });
-  res.headers.set('Access-Control-Allow-Origin', '*');
-  res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.headers.set('Access-Control-Max-Age', '86400');
-  return res;
+    const res = NextResponse.json(data, { status });
+    res.headers.set('Access-Control-Allow-Origin', '*');
+    res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.headers.set('Access-Control-Max-Age', '86400');
+    return res;
 }
 
 export async function DELETE(req, { params }) {
     const { table, id } = params;
     try {
+        // fetch record to get image url
+        const { rows } = await pool.query(
+            `SELECT image_url FROM ${table} WHERE id = $1::uuid;`,
+            [id]
+        );
+
+        if (rows.length !== 0) {
+            const image_url = rows[0].image_url;
+            if (image_url) {
+                try {
+                    await del(image_url);
+                } catch (err) {
+                    console.warn("Blob not found or already deleted:", image_url);
+                }
+            }
+        }
+
+
+
         const result = await pool.query(
             `DELETE FROM ${table} WHERE id = $1::uuid RETURNING *;`,
             [id]
         );
+
         if (result.rowCount === 0) {
             return createCorsResponse({ message: 'No row found' }, 404);
         }
